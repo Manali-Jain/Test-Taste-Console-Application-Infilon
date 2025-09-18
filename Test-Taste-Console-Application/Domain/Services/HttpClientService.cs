1194e0184507a95183ea8e94142ba9d4fa25e228
Manali <manalijain26798@gmail.com>
2025-09-18 15:34:35 +0530

    Add project files.

---
+++ Test-Taste-Console-Application/Domain/Services/HttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Test_Taste_Console_Application.Constants;
+
+namespace Test_Taste_Console_Application.Domain.Services
+{
+    ///<summary>
+    /// A service to create the HttpClient. 
+    ///</summary>
+    public class HttpClientService
+    {
+        public HttpClient Client { get; }
+
+        public HttpClientService(HttpClient client)
+        {
+            //The HTTP client is configured in the constructor.
+            Client = client;
+            Client.BaseAddress = new Uri(UriPath.BaseUri);
+            Client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue(HttpClientSettings.JsonType));
+            client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", "44bb3f57-b906-464a-bcc6-a2c0ac0af5f2");
+        }
+    }
+}
